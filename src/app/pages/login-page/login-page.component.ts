c5d21cfad0959cc6bd187e317c6d4a93a521fc99
Piotr Rasiński <246764@student.pwr.edu.pl>
2020-10-25 01:33:38 +0200

    w koncu ten routing

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-page',
@@ -7,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {}
 
   onSubmit() {}
 
+  btnClick= function () {
+    this.router.navigateByUrl('/register');
+};
 }
